07ff90c79a1d75086149ca614becbca0ce366e95
Steve Martin <stevemartin041@gmail.com>
2021-08-11 00:35:14 -0400

    Added windowing parameter to geks()

--- R/geks.R
+++ R/geks.R
@@ -1,11 +1,11 @@
-geks_matrix <- function(index, price, quantity, period, product, n, nper, na.rm) {
+geks_matrix <- function(index, price, quantity, product, n, nper, na.rm) {
   # making base prices/quantities is the slowest part of the calculation;
   # the algorithm calculates the lower-triangular part of the GEKS matrix
   # to avoid making relatives with different bases, then uses the 
   # time-reversal property of the 'index' function
   rows <- seq_len(nper)
   lt <- lapply(rows, function(i) {
-    if (i <= max(nper - n, 1)) return(rep_len(1, nper))
+    if (i < max(nper - n, 2)) return(rep_len(1, nper))
     js <- seq_len(i - 1)
     pad <- rep_len(1, nper - i + 1)
     # matching is only done for the lower-triangular part of the matrix
@@ -16,6 +16,7 @@ geks_matrix <- function(index, price, quantity, period, product, n, nper, na.rm)
     c(unlist(ans, use.names = FALSE), pad)
   })
   res <- do.call(rbind, lt)
+  rownames(res) <- colnames(res) <- names(price)
   # exploit time-reversal
   res[upper.tri(res)] <- 1 / t(res)[upper.tri(res)]
   res
@@ -23,25 +24,34 @@ geks_matrix <- function(index, price, quantity, period, product, n, nper, na.rm)
 
 geks <- function(f) {
   index <- match.fun(f)
-  function(price, quantity, period, product, n = nlevels(period), na.rm = FALSE) {
+  function(price, quantity, period, product, window = nlevels(period), n = window - 1, na.rm = FALSE) {
     if (gpindex:::different_lengths(price, quantity, period, product)) {
       stop("'price', 'quantity', 'period', and 'product' must be the same length")
     }
     period <- as.factor(period)
     nper <- nlevels(period)
-    if (!nper) return(structure(numeric(0), names = character(0)))
+    if (!nper) return(list())
+    window <- to_scalar(window)
     n <- to_scalar(n)
+    if (window < 2) stop("'window' must be greater than or equal to 2")
+    if (window > nper) stop("'window' must be less than the number of levels in 'period'")
     if (n < 1) stop("'n' must be greater than or equal to 1")
-    if (n > nper) stop("'n' must be less than the number of levels in 'period'")
+    if (n > window - 1) stop("'n' must be less than 'window' minus 1")
     price <- split(price, period)
     quantity <- split(quantity, period)
     product <- as.factor(product)
     attributes(product) <- NULL # faster to match on integer codes
     product <- split(product, period)
-    mat <- geks_matrix(index, price, quantity, period, product, n, nper, na.rm)
-    keep <- seq(nper - n + 1, nper)
-    res <- apply(mat[, keep, drop = FALSE], 2, geometric_mean, na.rm = na.rm)
-    structure(res / res[1], names = levels(period)[keep])
+    windows <- rolling_window(nper, window)
+    res <- vector("list", length(windows))
+    keep <- seq(window - n, window)
+    for (i in seq_along(res)) {
+      w <- windows[[i]]
+      mat <- geks_matrix(index, price[w], quantity[w], product[w], n, window, na.rm)
+      mat <- apply(mat[, keep, drop = FALSE], 2, geometric_mean, na.rm = na.rm)
+      res[[i]] <- mat[-1] / mat[-length(mat)]
+    }
+    res
   }
 }
 
--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,3 +1,8 @@
 to_scalar <- function(x) {
   trunc(as.numeric(x[1]))
 }
+
+rolling_window <- function(n, window) {
+  steps <- seq_len(n - window + 1)
+  Map(seq, from = steps, length.out = window)
+}
